894a7cd9886ca6a12d71350f50cf533ceb6995c5
Estiven Alvarez <Estiven.Alvarez@TRANESMEDELLIN.LOCAL>
2025-05-24 17:03:36 -0500

    finish chat

--- server/app.js
+++ server/app.js
@@ -1,39 +1,80 @@
-const dotenv = require('dotenv');
-const { createClient } = require('@libsql/client');
-const express = require('express');
-const http = require('http');
+import express from 'express'
+import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
-dotenv.config();
-const app = express();
-const server = http.createServer(app);
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
-// Servir archivos estáticos primero
-app.use(express.static('node_modules/bootstrap/dist'));
+dotenv.config()
+
+const port = process.env.PORT ?? 3000
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
 
 const db = createClient({
   url: 'libsql://dbasamblea-tibencode.aws-us-east-1.turso.io',
   authToken: process.env.DB_TOKEN
-});
+})
 
-(async () => {
-  try {
-    await db.execute(`
-      CREATE TABLE IF NOT EXISTS messages (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        content TEXT,
-        user TEXT
-      )
-    `);
-    console.log('Tabla messages verificada/creada');
-  } catch (err) {
-    console.error('Error creando/verificando la tabla:', err);
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user TEXT
+  )
+`)
+
+io.on('connection', async (socket) => {
+  console.log('a user has connected!')
+
+  socket.on('disconnect', () => {
+    console.log('an user has disconnected')
+  })
+
+  socket.on('chat message', async (msg) => {
+    let result
+    const username = socket.handshake.auth.username ?? 'anonymous'
+    console.log({ username })
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content, user) VALUES (:msg, :username)',
+        args: { msg, username }
+      })
+    } catch (e) {
+      console.error(e)
+      return
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
+  })
+
+  if (!socket.recovered) { // <- recuperase los mensajes sin conexión
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content, user FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString(), row.user)
+      })
+    } catch (e) {
+      console.error(e)
+    }
   }
-})();
+})
+
+app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-  res.sendFile(process.cwd() + '/client/index.html');
-});
+  res.sendFile(process.cwd() + '/client/index.html')
+})
 
-server.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
-});
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
